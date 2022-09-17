e0b487959c1d6c7abbacdc778647b510d3a0631d
Bhavish Dhanda <bhavish.dhanda@faircg.com>
2022-09-17 17:26:05 +1000

    Need to start stat collection

---
+++ app3.py
@@ -0,0 +1,8 @@
+ALEXA_TOP_1M = 'http://s3.amazonaws.com/alexa-static/top-1m.csv.zip'
+
+import pandas as pd
+
+top_sites = pd.read_csv(ALEXA_TOP_1M, header=None)
+
+print(top_sites.head())
+
---
+++ app4.py
@@ -0,0 +1,4 @@
+# find the number of scam alerts recieved from BTC who is WHO
+# Cross check that with BTCAbuseDB to see what it says
+# Create stats on the number of scam alerts recieved from BTC who is WHO based on what AbuseDB say 
+# Ie these many legal addresses have scam alerts (False Positives) and these many illegal addresses have scam alerts (True Positives)
