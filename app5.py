9fc3f85d618cab3a9eb65900adce42a9e5404551
Bhavish Dhanda <bhavish.dhanda@faircg.com>
2022-09-30 19:36:35 +1000

    Getting Neighbour data

--- app5.py
+++ app5.py
@@ -1,11 +1,14 @@
 import mysql.connector
 import requests
 from bs4 import BeautifulSoup
+from progressbar import Percentage, ProgressBar,Bar,ETA
 import pandas as pd
 
 db = mysql.connector.connect(user='root', password='1234', host='Bhavishs-MacBook-Air.local')
 dbCursor = db.cursor()
 dbCursor.execute("USE btc;")
+
+pbar = ProgressBar(widgets=[Bar('>', '[', ']'), ' ',Percentage(), ' ',ETA()])
 
 def extractHTML(address) : 
     btcWhoIsWhoUrl = "https://www.bitcoinwhoswho.com/address/" + address
@@ -34,12 +37,48 @@ def findIfBtcWhoIsWhoHasReport(address) :
                                                 numScamAlerts = len(x.find_all("div", {"class" :lambda x: x and x.startswith('row row_')}))/2
                                                 for z in x.find_all("div", {"class" :lambda x: x and x.startswith('row row_')}) :
                                                     finalSection.append(z.find_all("div", {"class" : "col-md-11"}))
-    print(address)
     return {"numScamAlerts" : numScamAlerts, "finalSection" : finalSection}
 
+def extractNeighbours(address) :
+    addressUrl = "https://blockchain.info/rawaddr/" + address
+    data = requests.request("GET", addressUrl).json()
+    result = []
+    if (data.__contains__("txs")):
+        for x in data["txs"]:
+            for y in x["out"]:
+                if(y.__contains__("addr")):
+                    if(y["addr"] is not None):
+                        result.append(y["addr"])
+            for y in x["inputs"]:
+                if(y.__contains__("addr")):
+                    if(y["addr"] is not None):
+                        result.append(y["addr"])
+    return result
+    
+def writeToFile(addressData, x) :
+    with open("./finalAddressAndNeighbourData/" + str(x) + ".html", "w") as outfile :
+        outfile.write(str(addressData))
 
-
-# dbCursor.execute("SELECT count(distinct(address)) FROM bitcoinheistdata;")
+def validateResults(addresses) :
+    addresses = []
+    print(len(maliciousAddressesToInvestigate))
+    for x in pbar(addresses) :
+        print(x)
+        addressData = []
+        result = findIfBtcWhoIsWhoHasReport(x)
+        if (result['numScamAlerts'] > 0) :
+            addresses.append(x)
+            addressData.append(result['finalSection'])
+            # Check NeighBourData
+            neighbours = extractNeighbours(x)
+            for x in neighbours :
+                result = findIfBtcWhoIsWhoHasReport(x)
+                neighBourData = findIfBtcWhoIsWhoHasReport(neighbours)
+                if (neighBourData['numScamAlerts'] > 0) :
+                    addressData.append(neighBourData['finalSection'])
+                writeToFile(addressData, x)
+    return {"addresses" : addresses}
+        
 dbCursor.execute("SELECT label,count(*) FROM bitcoinheistdata group by label;")
 
 result = dbCursor.fetchall()
@@ -52,7 +91,7 @@ legitAddressesToInvestigate = []
 
 legitAddresses = dbCursor.fetchall()
 
-for x in legitAddresses :
+for x in set(legitAddresses) :
     legitAddressesToInvestigate.append((str(x)).replace("(''","").replace("',)",""))
 
 
@@ -61,10 +100,21 @@ dbCursor.execute("SELECT address FROM bitcoinheistdata where label != 'white';")
 
 maliciousAddresses = dbCursor.fetchall()
 
+print(len(maliciousAddresses))
+
 maliciousAddressesToInvestigate = []
 
-for x in maliciousAddresses :
+for x in set(maliciousAddresses) :
     maliciousAddressesToInvestigate.append((str(x)).replace("('","").replace("',)",""))
 
-validateResults(legitAddressesToInvestigate)
-validateResults(maliciousAddressesToInvestigate)
+
+maliciousWithScamData = validateResults(maliciousAddressesToInvestigate)
+print("Malicious Addresses with Scams : " + str(maliciousWithScamData['addresses']))
+TruePositives = len(maliciousWithScamData['addresses'])/len(maliciousAddressesToInvestigate)
+print("True Positives : " + str(TruePositives))
+
+
+# legitWithScams = validateResults(legitAddressesToInvestigate)
+# print("Legit Addresses with Scams : " + str(legitWithScams))
+# FalsePositives = legitWithScams/len(legitAddressesToInvestigate)
+# print("False Positives : " + str(FalsePositives))
