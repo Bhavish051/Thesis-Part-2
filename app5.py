09d0ffbb52c2f228e95a8ed933c5fc62ea25812d
Bhavish Dhanda <bhavish.dhanda@faircg.com>
2022-10-08 17:23:18 +1100

    Changed

--- app5.py
+++ app5.py
@@ -20,25 +20,27 @@ def extractHTML(address) :
 def findIfBtcWhoIsWhoHasReport(address) : 
     html = extractHTML(address)
     parsedHtml = BeautifulSoup(html, "html.parser")
-    res = parsedHtml.body.find_all("div", {"id": "wrapper"})
+    if parsedHtml.body is not None :
+        res = parsedHtml.body.find_all("div", {"id": "wrapper"})
     # Loop now
-    finalSection = []
-    numScamAlerts = 0
-    for result in res : 
-        for d in result.find_all("section", {"id": "content"}) :
-            for x in d.find_all("div", {"id" : "search_address_index", "class" : "container"}) :
-                for y in x.find_all("div", {"class" : "row"}) :
-                    for z in y.find_all("div", {"class" : "col-lg-12"}) :
-                        for w in z.find_all("div", {"class" : "row"}) :
-                            for z in w.find_all("div", {"class" : "col-lg-12 float_left_box"}) :
-                                for x in z.find_all("div", {"class" : "row text-center"}) :
-                                    for z in x.find_all("div", {"class" : "col-lg-12"}) :
-                                        for y in z.find_all("div", {"class" : "float_left_box flb_scam_records_table"}) :
-                                            for x in y.find_all("div", {"class" : "collapse", "id" : "scam_records_table"}) :
-                                                # for z in x.find_all("div", {"class" : "row row_odd hide", "id":lambda x: x and x.startswith('scam_info')}) :
-                                                numScamAlerts = len(x.find_all("div", {"class" :lambda x: x and x.startswith('row row_')}))/2
-                                                for z in x.find_all("div", {"class" :lambda x: x and x.startswith('row row_')}) :
-                                                    finalSection.append(z.find_all("div", {"class" : "col-md-11"}))
+        finalSection = []
+        numScamAlerts = 0
+        for result in res : 
+            for d in result.find_all("section", {"id": "content"}) :
+                for x in d.find_all("div", {"id" : "search_address_index", "class" : "container"}) :
+                    for y in x.find_all("div", {"class" : "row"}) :
+                        for z in y.find_all("div", {"class" : "col-lg-12"}) :
+                            for w in z.find_all("div", {"class" : "row"}) :
+                                for z in w.find_all("div", {"class" : "col-lg-12 float_left_box"}) :
+                                    for x in z.find_all("div", {"class" : "row text-center"}) :
+                                        for z in x.find_all("div", {"class" : "col-lg-12"}) :
+                                            for y in z.find_all("div", {"class" : "float_left_box flb_scam_records_table"}) :
+                                                for x in y.find_all("div", {"class" : "collapse", "id" : "scam_records_table"}) :
+                                                    # for z in x.find_all("div", {"class" : "row row_odd hide", "id":lambda x: x and x.startswith('scam_info')}) :
+                                                    numScamAlerts = len(x.find_all("div", {"class" :lambda x: x and x.startswith('row row_')}))/2
+                                                    for z in x.find_all("div", {"class" :lambda x: x and x.startswith('row row_')}) :
+                                                        finalSection.append(z.find_all("div", {"class" : "col-md-11"}))
+    
     return {"numScamAlerts" : numScamAlerts, "finalSection" : finalSection}
 
 def extractNeighbours(address) :
@@ -99,8 +101,9 @@ legitAddressesToInvestigate = []
 
 legitAddresses = dbCursor.fetchall()
 
-for x in set(legitAddresses) :
-    legitAddressesToInvestigate.append((str(x)).replace("(''","").replace("',)",""))
+if legitAddresses is not None : 
+    for x in set(legitAddresses) :
+        legitAddressesToInvestigate.append((str(x)).replace("(''","").replace("',)",""))
 
 
 dbCursor.execute("select address from bitcoinheistdata where label = 'montrealSamSam' order by neighbors asc limit 100;")
@@ -109,9 +112,9 @@ dbCursor.execute("select address from bitcoinheistdata where label = 'montrealSa
 maliciousAddresses = dbCursor.fetchall()
 
 maliciousAddressesToInvestigate = []
-
-for x in maliciousAddresses :
-    maliciousAddressesToInvestigate.append((str(x)).replace("('","").replace("',)",""))
+if maliciousAddresses is not None :
+    for x in maliciousAddresses :
+        maliciousAddressesToInvestigate.append((str(x)).replace("('","").replace("',)",""))
 
 
 maliciousWithScamData = validateResults(set(maliciousAddressesToInvestigate))
